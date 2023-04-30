caa97f5133bc4fd9fad64cdf416310c6e57ab684
franco <ponzio31@gmail.com>
2023-04-30 15:02:18 -0300

    Fix git hub img path

--- scripts/products.js
+++ scripts/products.js
@@ -32,13 +32,13 @@ function productCardHTML(stockProduct, cartProductData){
 
 // Default products:
 let stockProducts = [
-    new Product("0", "Producto 0", 10, 3, "./img/bx-shopping-bag.svg"),
-    new Product("1", "Producto 1", 20, 10, "./img/bx-shopping-bag.svg"),
-    new Product("2", "Producto 2", 250, 2, "./img/bx-shopping-bag.svg"),
-    new Product("3", "Producto 3", 40, 5, "./img/bx-shopping-bag.svg"),
-    new Product("4", "Producto 4", 80, 20, "./img/bx-shopping-bag.svg"),
-    new Product("5", "Producto 5", 30, 10, "./img/bx-shopping-bag.svg"),
-    new Product("6", "Producto 6", 110, 3, "./img/bx-shopping-bag.svg"),
+    new Product("0", "Producto 0", 10, 3, "https://francoponzio31.github.io/JS---eCommerce/img/bx-shopping-bag.svg"),
+    new Product("1", "Producto 1", 20, 10, "https://francoponzio31.github.io/JS---eCommerce/img/bx-shopping-bag.svg"),
+    new Product("2", "Producto 2", 250, 2, "https://francoponzio31.github.io/JS---eCommerce/img/bx-shopping-bag.svg"),
+    new Product("3", "Producto 3", 40, 5, "https://francoponzio31.github.io/JS---eCommerce/img/bx-shopping-bag.svg"),
+    new Product("4", "Producto 4", 80, 20, "https://francoponzio31.github.io/JS---eCommerce/img/bx-shopping-bag.svg"),
+    new Product("5", "Producto 5", 30, 10, "https://francoponzio31.github.io/JS---eCommerce/img/bx-shopping-bag.svg"),
+    new Product("6", "Producto 6", 110, 3, "https://francoponzio31.github.io/JS---eCommerce/img/bx-shopping-bag.svg"),
 ];
 
 
